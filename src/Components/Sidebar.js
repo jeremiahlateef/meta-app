0402ca3a6f0fc926a1395500e0decf83b1f84b86
unknown <lateefjeremiah@gmail.com>
2022-11-15 02:00:44 +0100

    Pages: created the sidebar page

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Icon from "../Assets/icon-close-menu.svg";
+import { useGlobalContext } from "../context";
+
+export const Sidebar = () => {
+  const { isSidebarOpen, openModal, closeSidebar } = useGlobalContext();
+  return (
+    <div
+      className={`${
+        isSidebarOpen ? ` modal-overlay` : `modal-overlay close-modal`
+      }`}
+    >
+      <div
+        className={`${isSidebarOpen ? ` sidebar` : `sidebar close-sidebar`}`}
+      >
+        <button className="close-btn" onClick={closeSidebar}>
+          <img src={Icon} alt="close menu icon" className="close-menu" />
+        </button>
+        <div className="sidebar-lists">
+          <Link to="/" className="links" onClick={closeSidebar}>
+            Home
+          </Link>
+          <Link to="/place" className="links" onClick={closeSidebar}>
+            Place to stay
+          </Link>
+          <Link to="#" className="links">
+            NFTs
+          </Link>
+          <Link to="#" className="links">
+            Community
+          </Link>
+          <button
+            className="btn connect sidebar-button"
+            id="btn-connect"
+            onClick={() => {
+              closeSidebar();
+              openModal();
+            }}
+          >
+            Connect wallet
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
